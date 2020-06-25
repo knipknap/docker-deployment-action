14ed8d9ab774384e528a62f28520c7c312e72e50
Samuel <knipknap@gmail.com>
2020-06-26 00:38:00 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -49,7 +49,7 @@ fi
 case $INPUT_DEPLOYMENT_MODE in
 
   docker-swarm)
-    DEPLOYMENT_COMMAND=". $INPUT_DEPLOY_PATH/stacks/.env && docker"
+    DEPLOYMENT_COMMAND="cd $INPUT_DEPLOY_PATH; docker-compose -f ${INPUT_STACK_FILE_NAME} config 2>/dev/null | docker"
     STACK_FILE=$INPUT_DEPLOY_PATH/${INPUT_STACK_FILE_NAME}
 
     ([[ "$INPUT_COPY_STACK_FILE" = "true" ]] && STACK_FILE="$INPUT_DEPLOY_PATH/$STACK_FILE" ) ||
